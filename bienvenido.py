992e7a7833b1e4edc612b686ede844095f262de3
alulab <alulab@V20705.lab.inf.pucp.edu.pe>
2017-09-30 12:48:52 -0500

    iniciando fedora

---
+++ bienvenido.py
@@ -0,0 +1,11 @@
+#-*- encoding: utf-8 -*-
+name=raw_input("¿Cual es tu nombre?")
+print("welcome "+name +"!!")
+apellido=raw_input("¿cual es tu apellido?")
+print ("nombre completo " + name+ " " + apellido+"!!")
+
+
+
+
+
+
---
+++ suma.py
@@ -0,0 +1,4 @@
+var1=input("ingresa 1er numero")
+var2=input("ingresa 2do numero")
+sum = var1 + var2
+print ("suma total",sum) 
